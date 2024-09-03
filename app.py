33e6373e3fc15671f3f1adbf8f0c473304379acd
Tam <enquiretam@gmail.com>
2024-09-03 17:46:35 +0100

    updated models

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///property_db.db'
+app.config['SECRET_KEY'] = 'a_secret_key'  
 db = SQLAlchemy(app)
 
-from routes import *
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from models.user import User
+from models.property import Property
+from routes import auth_routes, property_routes
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.before_first_request
+def create_tables():
+    db.create_all() 
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -3,8 +3,9 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    first_name = db.Column(db.String(80), unique=True, nullable=False)
-    last_name = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     properties = db.relationship('Property', backref='owner', lazy=True)
 
